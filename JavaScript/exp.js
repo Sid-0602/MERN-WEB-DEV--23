cbd11e101e94e2e3f82d3a557dbaa6e3b59c750f
Siddhant J <sidbnjadhav@gmail.com>
2023-05-03 13:08:22 +0530

    JavaScript push

---
+++ JavaScript/exp.js
@@ -0,0 +1,16 @@
+var a = 1/0;
+console.log(a);
+
+
+pow=Math.pow(10,3);
+console.log(pow);
+
+//this is parseINT function which converts string to int.
+
+int_to_string = parseInt("20");
+console.log(int_to_string);
+
+
+//typeof operator: 
+console.log("This is typeof Operator: ");
+console.log(typeof(10));
---
+++ JavaScript/index.js
@@ -0,0 +1,27 @@
+var a = 10;
+console.log("This is 'a': ",a);
+
+var b = 5;
+console.log("This is 'b': ",b);
+var c= a+b;
+
+console.log("This is addition of 'c': ",c);
+
+
+
+var d = 1/0;
+console.log(d);
+
+
+pow=Math.pow(10,3);
+console.log(pow);
+
+//this is parseINT function which converts string to int.
+
+int_to_string = parseInt("20");
+console.log(int_to_string);
+
+
+//typeof operator: 
+console.log("This is typeof Operator: ");
+console.log(typeof(10));
