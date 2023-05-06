8289e135381766d3751bae28083f20ca2dd918a9
Siddhant J <sidbnjadhav@gmail.com>
2023-05-06 14:07:10 +0530

    functions in js

---
+++ JavaScript/Functions and Arrays/functions.js
@@ -0,0 +1,14 @@
+
+/* Function intialise */
+function showAlert(message){
+    alert(message);
+}
+  
+  
+function sum_of_num(a,b){
+    return a+b;
+}
+  
+showAlert("Hello JS!!");
+var result = sum_of_num(4,6);
+console.log(result);
