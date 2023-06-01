6d7b80a343ed6041bd634de6e21f69c87649e1dc
Siddhant J <sidbnjadhav@gmail.com>
2023-06-01 12:59:22 +0530

    Function expressions: 1. Named Function 2. Unnamed Function

---
+++ JavaScript/Functions and Arrays/fun_exp.js
@@ -0,0 +1,29 @@
+//Function expressions is way to create a function. 
+
+
+var factorial = function fact(n){  //this is named function exp.
+
+    var ans=1;
+    for(var i=0;i<n;i++){
+        ans = ans*i;
+    }
+
+    return ans;
+
+};
+
+var factorial2 = function (n){  //this is unnamed/anonymous  function exp.
+
+    if(n==0){
+        return 1;
+    }
+
+    return n * factorial2(n-1);
+
+};
+
+
+console.log(factorial);
+console.log(factorial2);
+console.log(factorial(5));
+console.log(factorial2(6));
