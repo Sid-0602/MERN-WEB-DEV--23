8241bb84d5903ea1ca4fd9104d6462eafd58fc47
Siddhant J <sidbnjadhav@gmail.com>
2023-06-03 13:38:48 +0530

    Objects in JS

---
+++ JavaScript/Objects_Timings/objects.js
@@ -0,0 +1,13 @@
+var student={
+    "name":"abc", 
+     rollNo: 135, 
+     marks:90
+}; //This is key-value pair in form of object.
+
+console.log(student);
+student.marks=100; // accessing/changing the property of object.
+console.log(student);
+
+console.log(student.sem);
+student.sem=6; //adding a new key/field into the object. 
+console.log(student);
