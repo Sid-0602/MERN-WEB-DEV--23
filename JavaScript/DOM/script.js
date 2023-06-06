b8ff1579a56b3380fe5fdaacb501ec659f7a167b
Siddhant J <sidbnjadhav@gmail.com>
2023-06-06 14:53:37 +0530

    Adding Ecternal JS files

---
+++ JavaScript/DOM/script.js
@@ -0,0 +1,18 @@
+//this is console part.
+console.log("Hello! This is Console");
+
+function say_hello(){
+    alert('Hello Bachoooo');
+}
+
+function sayBye(){
+    alert('Byee!!');
+}
+
+var hellobtn = document.getElementById('btn2');
+console.log(hellobtn);
+hellobtn.addEventListener('click',say_hello);
+hellobtn.addEventListener('click',sayBye); //there are 2 event listners for this button.
+
+
+
