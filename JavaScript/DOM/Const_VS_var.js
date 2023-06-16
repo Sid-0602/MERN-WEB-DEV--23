acb076f1f86235a4dcea6d122971d91cdd2c29e8
Siddhant J <sidbnjadhav@gmail.com>
2023-06-16 11:54:39 +0530

    Const VS var

---
+++ JavaScript/DOM/Const_VS_var.js
@@ -0,0 +1,14 @@
+var a=10;
+console.log(a);
+a=20;
+console.log(a);
+
+const b=30;
+console.log(b);
+// Not possible to write : b=11;
+
+// Allocating value of c anytime after is not allowed.
+// const c;
+
+// Variable Hoisting in JS is not allowed. It should be referenced first before it is accessed.
+
