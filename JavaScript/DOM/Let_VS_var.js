41998e4c7654c5335ae63c37ccdfa0a4e636dbd3
Siddhant J <sidbnjadhav@gmail.com>
2023-06-16 12:40:37 +0530

    LET VS VAR

--- JavaScript/DOM/Let_VS_var.js
+++ JavaScript/DOM/Let_VS_var.js
@@ -13,12 +13,37 @@ console.log(a,b,c);
 //LET is used to create variables in block scope: 
  
 console.log("!! This is for LET !!")
-
+/*
 if(a<=10){
-    //local scope of m.
     let m="This is m";
 }else{
     let n="This is n";
 }
 
-console.log(a,m,n);
+console.log(a,m,n); //This is not allowed as m, n are in local scope.
+
+*/
+
+
+
+// LET VS VAR : 
+
+
+console.log("For VAR for loop: ");
+for(var i=0;i<5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },1000);
+}
+
+
+for(let i=0;i<5;i++){
+
+    //every setInterval has each of its own i value.
+    setTimeout(function(){
+        if(i==0){
+            console.log("For LET for loop: ");
+        }
+        console.log(i);
+    },1000);
+}
