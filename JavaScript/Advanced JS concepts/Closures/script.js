272b541f8b129667927a72e9d818de9f1ed1789f
Siddhant J <sidbnjadhav@gmail.com>
2023-06-23 23:21:55 +0530

    Closures in JS

--- JavaScript/Advanced JS concepts/Closures/script.js
+++ JavaScript/Advanced JS concepts/Closures/script.js
@@ -5,11 +5,51 @@ function outer(){
     console.log(i,j);
     var inner = function(){
         var k=30;
-        console.log(j,k);
+        console.log(i,j,k);
+        j++;
+        k++;
+        i++;
     }
     return inner;
 }
 
 
-var inner = outer(); //at this point, outer function is completely executed. 
-inner(); //this function call should not return but due to clousers, it will.
+var inner= outer(); //at this point, outer function is completely executed. 
+inner(); //this function call should not return but due to closures, it will.
+
+inner();  //this will give k=30, as both function calls will produce k value as 30 initially.
+          // this will give j=20 for first call and j=21 for second call, as j is in scope of outer function. 
+
+inner(); //j=22.
+
+
+//this has global scope as we have declared it with var.
+
+function test1(){
+for(var i=1;i<=5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },1000)
+}
+
+console.log("After for loop: ",i);
+}
+
+test1(); 
+
+
+//this i has local scope due to "let" variable.
+
+function test2(){
+    setTimeout(function(){
+        console.log("This is for LET variable!!");
+    },1000); 
+for(let i=1;i<=5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },1000)
+}
+
+}
+
+test2();
