2ddc80bfe6fac96b19681cdf1e9587723481f073
Siddhant J <sidbnjadhav@gmail.com>
2023-06-24 19:08:17 +0530

    Binding in JS

--- JavaScript/Advanced JS concepts/Constructors & Prototype/this.js
+++ JavaScript/Advanced JS concepts/Constructors & Prototype/this.js
@@ -11,3 +11,34 @@ function Vechile(name){
 
 new Vechile('Car');
 
+// Rule 2: "Explicit Binding": 
+
+const john = {
+    name: "John",
+};
+
+function ask(){
+    console.log(this,this.name);
+}
+
+ask.call(john); //call function is asking ask() function to call john object.
+ask.apply(john);
+
+
+//Hard Binding Rule: 
+
+var raj={
+    name: 'Raj',
+    greet: function(){
+        console.log('Hello',this);
+    }
+}
+
+raj.greet();
+var localAsk = raj.greet;
+localAsk();//this will give us output of window as it is called in global scope. Instead use hard-binding for this.
+
+var localGreet = raj.greet.bind(raj); //this is hard-binded to "raj" object.
+localGreet();
+//It can be used for other functions like setTimeOut:
+setTimeout(raj.greet.bind(raj),1000);
