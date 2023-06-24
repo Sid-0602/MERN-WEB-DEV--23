352872be04f6ad1aad6af8427b889624055405bb
Siddhant J <sidbnjadhav@gmail.com>
2023-06-24 13:39:46 +0530

    this JS code file

---
+++ JavaScript/Advanced JS concepts/Constructors & Prototype/this.js
@@ -0,0 +1,12 @@
+//There are 4 rules to determine the value of "this" keyword
+
+
+// Rule 1: "new" keyword
+
+//It returns Vechile object with empty value.
+function Vechile(){
+    console.log(this);
+}
+
+new Vechile();
+
