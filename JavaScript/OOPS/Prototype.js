952b24fb71459b2d636cfe10f1f5678795c99cd4
Siddhant J <sidbnjadhav@gmail.com>
2023-06-28 22:03:06 +0530

    Prototypes update

--- JavaScript/OOPS/Prototype.js
+++ JavaScript/OOPS/Prototype.js
@@ -8,10 +8,28 @@ function Vehicle(numWheels,price){
     this.price = price;
 
     //these functions will have behaviors.
-    this.getPrice = function(){
+    /*this.getPrice = function(){
         return this.price;
     }
+    */
 }
+
+Vehicle.prototype.getPrice = function(){
+    return this.price;
+};
+
 
 console.log(Vehicle.prototype); //this will print the constructor and __proto__ 
 console.log(Vehicle.prototype.constructor); //this will return function "Vehicle".
+
+var Vehicle_1 = new Vehicle(2,50);
+var  Vehicle_2 = new Vehicle(4,800);
+
+
+Vehicle_1.getPrice();
+
+//Adding property in Run-time: 
+
+Vehicle.prototype.color = "Black";
+console.log(Vehicle_1.color);
+console.log(Vehicle_2.color);
