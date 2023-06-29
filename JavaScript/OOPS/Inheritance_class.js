ade68d88d8591a848b08ef5d8731355f4f238468
Siddhant J <sidbnjadhav@gmail.com>
2023-06-29 15:06:36 +0530

    Inheritance in JS

---
+++ JavaScript/OOPS/Inheritance_class.js
@@ -0,0 +1,33 @@
+//Inheritance from Vehicle class into "Car" class: 
+
+var Vehicle = class{
+    //cunstructor call: 
+    constructor(numWheels,price){
+        this.numWheels = numWheels;
+        this.price = price;
+    }
+
+    getPrice(){
+        return this.pricel
+    }
+
+    printDescription(){
+        console.log("Vehicle ",this.numWheels,this.price);
+    }
+}
+
+class Car extends Vehicle{
+    constructor(numDoors,price){
+        super(4,price);
+        this.numDoors = numDoors;
+    }
+    
+    printDescription(){
+        super.printDescription();
+        console.log("CAR");
+    }
+}
+
+var c = new Car(4,500);
+console.log(c);
+c.printDescription();
