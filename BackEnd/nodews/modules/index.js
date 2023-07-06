07a64725dbb98862ed87377999204b3f9509fc20
Siddhant J <sidbnjadhav@gmail.com>
2023-07-06 07:00:13 +0530

    index push

--- BackEnd/nodews/modules/index.js
+++ BackEnd/nodews/modules/index.js
@@ -1,5 +1,5 @@
 //A module is a reusable part of code that is later imported to main file with driver code. 
-const operations = require('./operations');
+const operations = require('./operations'); //require function is used to include node module and libraries.
 
 console.log(operations.add(2,3));
 console.log(operations.multiply(2,3));
