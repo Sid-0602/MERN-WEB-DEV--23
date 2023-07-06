e002dd35985367a0b120796005f6442399d2a602
Siddhant J <sidbnjadhav@gmail.com>
2023-07-06 07:40:53 +0530

    First HTML response with backend

--- BackEnd/nodews/BasicHttpServer/index.js
+++ BackEnd/nodews/BasicHttpServer/index.js
@@ -2,7 +2,16 @@ const http = require('http');
 const port = 8000;
 
 
-const server = http.createServer(); //this event creates server.
+//request handler: 
+
+function requestHandler(req,res){
+    console.log(req.url);
+    res.writeHead(200,{'content-type':'text/html'});
+    res.end("<h1>Gotcha!!</h1>");
+}
+
+//creating server and running it using server.listen.
+const server = http.createServer(requestHandler); //this event creates server.
 
 server.listen(port,function(err){
     if(err){
