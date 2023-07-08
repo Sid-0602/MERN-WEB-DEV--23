cdc59ba4016ddfc7f17495870d2e58b1ec208a50
Siddhant J <sidbnjadhav@gmail.com>
2023-07-08 17:13:21 +0530

    Contact List Update

--- BackEnd/Projects/Contact-List/index.js
+++ BackEnd/Projects/Contact-List/index.js
@@ -10,12 +10,42 @@ app.set('view engine','ejs');
 //setting up the view path.
 app.set('views',path.join(__dirname,'views'));
 
+var contactList = [
+    {
+        name:"Siddhant J",
+        phone:"8010099787"
+    },
+    {
+        name:"Siddharth S",
+        phone: "9333928772"
+    },
+    {
+        name: "Vaishnavi D",
+        phone: "8669973534"
+    }
+]
+
+
 
 //return a response for request: 
+
+//These are controllers: 
 app.get('/',function(req,res){
     //console.log(req); //this displays request data that is requested by browser.
 
-    return res.render('home',{title: "My-Contacts"});
+    return res.render('home',{
+        title: "Contacts-List",
+        contact_list : contactList
+    
+    });
+});
+
+app.get('/practise',function(req,res){
+    //console.log(req); //this displays request data that is requested by browser.
+
+    return res.render('practise',{
+        title: "Practice Page"
+    });
 });
 
 app.listen(port,function(err){
