55cca4fa4639c95bc04d1ce8b4c128d853852793
Siddhant J <sidbnjadhav@gmail.com>
2023-07-20 19:43:23 +0530

    Cookie add and test

--- ConnectIO/controllers/home_controller.js
+++ ConnectIO/controllers/home_controller.js
@@ -1,12 +1,17 @@
 
 //the syntax to create controller: 
-
+//setting up a cookie: 
 // module.exports.actionName = function(req,res).
 
 module.exports.home = function(req,res){
+    console.log(req.cookies); //this gives us cookie. 
+
+    // res.cookie('user_id',25); //changing value of cookie from server side.
     return res.render('../views/home.ejs',{
         title: "Home"
     });
 }
+
+
 
 
--- ConnectIO/index.js
+++ ConnectIO/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000; //when deployed on live server, it is deployed on port 80.
-const db = require('./config/mongoose');
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
-app.use(express.static('/ConnectIO/assets'));
 
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.use(express.static('/assets'));
 
 app.use(expressLayouts); //using layouts. 
 //extract styles and scripts from the layout. 
