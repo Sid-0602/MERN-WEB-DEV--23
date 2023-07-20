9c9f9ec9b16aa9ccae9999a6a0569c3c62d66b9e
Siddhant J <sidbnjadhav@gmail.com>
2023-07-20 19:02:28 +0530

    Sign in and login pages update push

--- ConnectIO/controllers/users_controller.js
+++ ConnectIO/controllers/users_controller.js
@@ -3,3 +3,19 @@ module.exports.profile = function(req,res){
         title: "User-Profile"
     })
 }
+
+
+//render the sign up page. 
+module.exports.signUp= function(req,res){
+    return res.render('../views/user_sign_up.ejs',{
+        title: "ConnectIO | Sign Up"
+    })
+}
+
+
+//render signIn page. 
+module.exports.signIn= function(req,res){
+    return res.render('../views/user_sign_in.ejs',{
+        title: "ConnectIO | Sign In"
+    })
+}
--- ConnectIO/routes/users.js
+++ ConnectIO/routes/users.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/users_controller');
+const usersController = require('../controllers/users_controller');
+
+//this router is used for user profile:
+router.get('/profile',usersController.profile);
+
+//these routers is used for user sign-in, sign-up:
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
 
 
-router.get('/profile',userController.profile)
+
+
 module.exports = router;
