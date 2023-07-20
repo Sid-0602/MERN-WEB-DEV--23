bf815980c05fea78e4095c04c48d904ed42a2b53
Siddhant Jadhav <86071680+Sid-0602@users.noreply.github.com>
2023-07-20 22:23:47 +0530

    Not manual auth for main branch feature

--- ConnectIO/controllers/users_controller.js
+++ ConnectIO/controllers/users_controller.js
@@ -54,34 +54,7 @@ module.exports.create = function(req,res){
 }
 
 //get the sign in data and create a session for user. 
-
-//This is local auth method in manual-local-auth: 
 module.exports.createSession = function(req,res){
     //steps for auth: 
     //find the user: 
-    User.findOne({email: req.body.email},function(err,user){
-        if(err){
-            console.log("Error in finding user in signing in!");
-        }
-        //handle user found: 
-        if(user){
-
-            //handle password which don't match: 
-            if(user.password!=req.body.password){
-                return res.redirect('back');
-            }
-
-            // handle session creation: 
-            res.cookie('user_id',user.id);
-            return res.redirect('/users/profile');
-            
-        }
-        else{
-            //handle user not found : 
-            return res.redirect('back');
-        }
-    })
-
-    
-
 }
