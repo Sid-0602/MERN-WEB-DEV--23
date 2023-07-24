172c35772740b3f0ade0ff9c6a539389679fe12b
Siddhant J <sidbnjadhav@gmail.com>
2023-07-24 20:28:54 +0530

    understanding Callbacks

--- BackEnd/nodews/modules/index.js
+++ BackEnd/nodews/modules/index.js
@@ -4,3 +4,18 @@ const operations = require('./operations'); //require function is used to includ
 console.log(operations.add(2,3));
 console.log(operations.multiply(2,3));
 console.log(operations.division(2,3));
+
+
+/* To understand function(req,res): */
+
+function something(callback){
+    var x = 5;
+    var y = 'anything';
+    callback(x,y);
+}
+
+
+something(function(req,res){
+    console.log(req);
+    console.log(res);
+})
