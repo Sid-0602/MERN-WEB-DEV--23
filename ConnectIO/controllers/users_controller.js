074167b3554a75608b77be64ff53c65839afd891
Siddhant J <sidbnjadhav@gmail.com>
2023-07-27 11:29:30 +0530

    Passport JS integrate and update

--- ConnectIO/controllers/users_controller.js
+++ ConnectIO/controllers/users_controller.js
@@ -10,6 +10,10 @@ module.exports.profile = function(req,res){
 
 //render the sign up page. 
 module.exports.signUp= function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    
     return res.render('../views/user_sign_up.ejs',{
         title: "ConnectIO | Sign Up"
     })
@@ -18,6 +22,10 @@ module.exports.signUp= function(req,res){
 
 //render signIn page. 
 module.exports.signIn= function(req,res){
+
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('../views/user_sign_in.ejs',{
         title: "ConnectIO | Sign In"
     })
