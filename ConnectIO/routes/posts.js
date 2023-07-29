7601ff794583f4515f9f3ad25f1f6782e18e61dd
Siddhant J <sidbnjadhav@gmail.com>
2023-07-29 06:56:44 +0530

    Posts backend complete

--- ConnectIO/routes/posts.js
+++ ConnectIO/routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const postsController = require('../controllers/posts_controller');
+const passport = require('passport');
 
-
-router.post('/create',postsController.create);
+//adding extra security using passport.checkAuthentication() 
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 
 
