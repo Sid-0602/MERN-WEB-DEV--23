b38786d5fe1cf6abd61be47274f187a55ccf6285
Siddhant J <sidbnjadhav@gmail.com>
2023-08-12 20:12:12 +0530

    AJAX for posts home

--- ConnectIO/assets/js/home_posts.js
+++ ConnectIO/assets/js/home_posts.js
@@ -1,6 +1,26 @@
-{
+{   
+    //method to submit form data for new post using ajax: 
+    let createPost = function(){
+        let newPostForm = $('#new-post-form');
+        newPostForm.submit(function(e){
+            e.preventDefault(); 
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+                    console.log(data);
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            })
+        });
+    }
+
+    //method to create a post in DOM
+
     
 
-    console.log("Hello from Home Posts!")
-        
+    createPost();
 }
--- ConnectIO/controllers/posts_controller.js
+++ ConnectIO/controllers/posts_controller.js
@@ -4,11 +4,21 @@ const Comment = require('../models/comment.js');
 
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
         
+
+        if(req.xhr){
+            return  res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!!"
+            });
+        };
         console.log("Posted Successfully!!");
         return res.redirect('back');
     }catch(err){
