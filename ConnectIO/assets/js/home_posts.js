e45c58d983ba89caad55ac3b5f7a208ff388f914
Siddhant J <sidbnjadhav@gmail.com>
2023-08-23 11:53:27 +0530

    POST Created using AJAX

--- ConnectIO/assets/js/home_posts.js
+++ ConnectIO/assets/js/home_posts.js
@@ -10,6 +10,9 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
+                    let newPost = newPostDom(data.data.post);
+                    //prepend it to list: 
+                    $('#post-list-container>ul').prepend(newPost);
                     console.log(data);
                 }, error: function(error){
                     console.log(error.responseText);
@@ -20,7 +23,31 @@
 
     //method to create a post in DOM
 
-    
+    let newPostDom = function(post){
+        return $(`
+        <li id="post- ${post._id}">
+            <h3>        
+                        ${post.content}
+                        <small>Author: ${ post.user.name }  </small>
+                        <small> | 
+                                <a href="/posts/destroy/${post._id}" class="delete-post-button">DEL</a>
+                        </small>
+            </h3>
 
+            <div class="post-comments">
+                        <form action="/comments/create" method="POST">
+                            <input type="text" name="content" placeholder="Type here to add comment..." required>
+                            <input type="hidden" name="post" value="${ post._id }" >
+                            <input type="submit" value="Add Comment">
+                        </form>
+            </div>
+
+            <div class="post-comments-list">
+                    <ol id="post-comments- ${post._id}">
+                    </ol>
+                </div>
+        </li>
+        `)
+    }
     createPost();
 }
