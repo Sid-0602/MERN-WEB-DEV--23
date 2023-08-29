1d56dd0abec8f764875ebd2217ced54dddef2fc0
Siddhant J <sidbnjadhav@gmail.com>
2023-08-29 21:47:11 +0530

    jwt authentication and authorization

--- ConnectIO/controllers/api/v1/posts_api.js
+++ ConnectIO/controllers/api/v1/posts_api.js
@@ -34,12 +34,17 @@ module.exports.destroy = async function(req,res){
     try{
         let post = await Post.findById(req.params.id);
         //.id means converting object id into string. _id is ObjectID. 
-        post.remove();
-        await Comment.deleteMany({post: req.params.id});
+        
+        if(post.user == req.user.id){
+            post.remove();
+            await Comment.deleteMany({post: req.params.id});
 
-        return  res.json(200,{
-            message:"Post deleted successfully!"
-        });;
+            return  res.json(200,{message:"Post deleted successfully!"});;
+        }else{
+            return  res.json(401,{message:"UnAuthorized Access!!"});;
+        }
+        
+        
     }catch(err){
 
         console.log("Error : ",err);
--- ConnectIO/routes/api/v1/posts.js
+++ ConnectIO/routes/api/v1/posts.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const postAPI = require('../../../controllers/api/v1/posts_api'); //this will use post api function from post.
+const passport = require('passport');
 
 router.get('/',postAPI.index);
-router.delete('/:id',postAPI.destroy);
+//authentication check using Passport JWT:
+router.delete('/:id',passport.authenticate('jwt',{session: false}),postAPI.destroy);
 module.exports = router;
