101538033b3802b40634816b81eea254d6852727
Siddhant J <sidbnjadhav@gmail.com>
2023-09-09 16:26:17 +0530

    Chatting Application complete push

--- ConnectIO/assets/js/chat_engine.js
+++ ConnectIO/assets/js/chat_engine.js
@@ -6,7 +6,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://localhost:6000');
 
         //if email is present, then call the event (connectionHandler)
         if(this.userEmail){
@@ -15,8 +15,53 @@ class ChatEngine{
     }
 
     connectionHandler(){
+
+        let self = this;
         this.socket.on('connect',function(){
             console.log('connection established using sockets');
+
+            //this is request sent for joining chat-room: 
+            self.socket.emit('join_room',{
+                user_email: self.userEmail,
+                chatroom : 'ConnectIO'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log("a user has joined!");
+            })
         });
+
+        //Sending a message (broadcasting it):
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom:'ConnectIO'
+                })
+            }
+        });
+
+        //to detect messages received:
+        self.socket.on('receive_message',function(data){
+            let newMessage = $('<li>');
+            let messageType = 'other-message';
+            
+            if(data.userEmail == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append('<span>',{
+                'html':data.message
+            });
+            newMessage.append('<sub>',{
+                'html': data.user_email
+            })
+
+            newMessage.addClass(messageType);
+            $('#chat-messages-list').append(newMessage);
+        })
     }
 }
--- ConnectIO/config/chat_sockets.js
+++ ConnectIO/config/chat_sockets.js
@@ -7,9 +7,22 @@ module.exports.chatSockets = function(socketServer){
 
     io.sockets.on('connection',function(socket){
         console.log("New connection received!!",socket.id);
-    })
+        io.sockets.on('disconnect',function(){
+            console.log("Connection is disconnected!!");
+        })
+        
+        //chat-room joining request: 
+        socket.on('join_room',function(data){
+            console.log('Joining request rec.',data);
+    
+            socket.join(data.chatroom);
 
-    io.sockets.on('disconnect',function(){
-        console.log("Connection is disconnected!!");
-    })
+            io.in(data.chatroom).emit('user_joined',data);
+        })
+
+        //receive message from client and broadcast it: 
+        socket.on('send-message',function(data){
+            io.in(data.chatroom).emit('receive_message',data);
+        })
+    });
 }
