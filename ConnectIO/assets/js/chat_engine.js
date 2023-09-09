810d6708b9787e81c02608f95a64ee9e78c12960
Siddhant J <sidbnjadhav@gmail.com>
2023-09-09 15:35:56 +0530

    Chat connection using Socket.io

--- ConnectIO/assets/js/chat_engine.js
+++ ConnectIO/assets/js/chat_engine.js
@@ -1,1 +1,22 @@
-//This file is communicating from the client side. 
+//This file is communicating from the client side.
+
+//This class send request for connection: 
+class ChatEngine{
+    constructor(chatBoxId,userEmail){
+        this.chatBox = $(`#${chatBoxId}`);
+        this.userEmail = userEmail;
+
+        this.socket = io.connect('http://localhost:5000');
+
+        //if email is present, then call the event (connectionHandler)
+        if(this.userEmail){
+            this.connectionHandler();
+        }
+    }
+
+    connectionHandler(){
+        this.socket.on('connect',function(){
+            console.log('connection established using sockets');
+        });
+    }
+}
--- ConnectIO/config/chat_sockets.js
+++ ConnectIO/config/chat_sockets.js
@@ -1,5 +1,15 @@
 //This file will be communicating through server (observer's side). 
 
+//this will receive connection requests: 
 module.exports.chatSockets = function(socketServer){
 
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection',function(socket){
+        console.log("New connection received!!",socket.id);
+    })
+
+    io.sockets.on('disconnect',function(){
+        console.log("Connection is disconnected!!");
+    })
 }
